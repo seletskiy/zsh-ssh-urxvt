19a8d0c0ec36233474891615d0bcd6abfdea951d
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-03-18 10:19:04 +0600

    make most complex aliases work
    node.x U -u sphinx -i -> ssh node.local sudo -u sphinx -i ->
        sphinx@node $
    
    node.x -l s.seletskiy -> ssh node.local -ls.seletskiy ->
        s.seletskiy@node $
    
    node.x -ls.seletskiy -> ssh node.local -ls.seletskiy ->
        s.seletskiy@node $
    
    node.x U -> ssh node.local sudo -i ->
        root@node #

--- plugin.zsh
+++ plugin.zsh
@@ -1,25 +1,44 @@
 alias ssh='ssh-urxvt'
 
 function ssh-urxvt() {
-    local opts_noargs="1246AaCfGgKkMNnqsTtVvXxYy"
-    local opts_args="b:c:D:E:e:F:I:i:L:l:m:O:o:p:Q:R:S:W:w:"
+    local opts_noarg="1246AaCfGgKkMNnqsTtVvXxYy"
+    local opts_arg="bcDEeFIiLlmOopQRSWw"
+
     local flags=()
     local positionals=()
 
     # parse ssh options and split flags from positional arguments
     while [ $# -gt 0 ]; do
-        OPTIND=0
-        while getopts "$opts_noargs$opts_args" opt "$@"; do
-            flags=($flags -$opt$OPTARG)
-        done
+        case "$1" in
+            -[$opts_noarg])
+                flags=($flags $1)
+                ;;
+            -[$opts_arg])
+                flags=($flags $1$2)
+                shift
+                ;;
+            -[$opts_arg]*)
+                flags=($flags $1)
+                ;;
+            -*)
+                echo unknown ssh flag: "$1"
+                return 1
+                ;;
+            *)
+                positionals=($positionals "$1")
+                ;;
+        esac
 
-        shift $(( $OPTIND - 1 ))
+        shift
 
-        if [ $# -gt 0 ]; then
-            positionals=($positionals "$1")
-            shift
+        if [ ${#positionals[@]} -ge 2 ]; then
+            break
         fi
     done
+
+    if [ $# -gt 0 ]; then
+        positionals=($positionals $@)
+    fi
 
     # in case of stdin, stdout or stderr is not a terminal, fallback to ssh
     if [[ ! ( -t 0 && -t 1 && -t 2 ) ]]; then
