6f2d70c0e986b33ea66f244ff6d73be0f7f2f634
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-03-18 10:19:04 +0600

    add ability to pass commands with TERM

--- plugin.zsh
+++ plugin.zsh
@@ -1,58 +1,37 @@
-alias ssh='ssh-urxvt'
+alias ssh=ssh-urxvt
 
 function ssh-urxvt() {
-    local opts_noarg="1246AaCfGgKkMNnqsTtVvXxYy"
-    local opts_arg="bcDEeFIiLlmOopQRSWw"
-
-    local flags=()
-    local positionals=()
-
-    # parse ssh options and split flags from positional arguments
-    while [ $# -gt 0 ]; do
-        case "$1" in
-            -[$opts_noarg])
-                flags=($flags $1)
-                ;;
-            -[$opts_arg])
-                flags=($flags $1$2)
-                shift
-                ;;
-            -[$opts_arg]*)
-                flags=($flags $1)
-                ;;
-            -*)
-                echo unknown ssh flag: "$1"
-                return 1
-                ;;
-            *)
-                positionals=($positionals "$1")
-                ;;
-        esac
-
-        shift
-
-        if [ ${#positionals[@]} -ge 2 ]; then
-            break
-        fi
-    done
-
-    if [ $# -gt 0 ]; then
-        positionals=($positionals $@)
-    fi
-
     # in case of stdin, stdout or stderr is not a terminal, fallback to ssh
     if [[ ! ( -t 0 && -t 1 && -t 2 ) ]]; then
-        \ssh "$@"
+        command ssh "$@"
         return
     fi
 
-    # if command sspeciified, fallback to ssh
-    if [ ${#positionals} -ge 2 ]; then
-        \ssh "${flags[@]}" "${positionals[@]}"
-        return
+    # for ## pattern in case
+    setopt local_options extended_glob
+
+    local interactive
+    local opts
+
+    # parse ssh options and split flags from positional arguments
+    zparseopts -a opts -D \
+        'b:' 'c:' 'D:' 'E:' 'e:' 'F:' 'I:' 'i:' 'L:' 'l:' 'm:' 'O:' 'o:' \
+            'p:' 'Q:' 'R:' 'S:' 'W:' 'w:' \
+        '1' '2' '4' '6' 'A' 'a' 'C' 'f' 'G' 'g' 'K' 'k' 'M' 'N' 'n' 'q' 's' \
+            'T' 'V' 'v' 'X' 'x' 'Y' 'y' \
+            't=interactive'
+
+    local shell='$SHELL'
+    if [ $# -ge 2 ]; then
+        if [ "$interactive" ]; then
+            shell+=" -ic ${(q)${(qqq@)@:2}}"
+        else
+            command ssh "${opts[@]}" "$@"
+            return $?
+        fi
     fi
 
     # check terminal is known, if not, fallback to xterm
-    \ssh -t "${flags[@]}" "$positionals" \
-        "infocmp >/dev/null 2>&1 || export TERM=xterm; LANG=$LANG \$SHELL"
+    command ssh -t "${opts[@]}" "$1" \
+        "infocmp >/dev/null 2>&1 || export TERM=xterm; LANG=$LANG exec $shell"
 }
